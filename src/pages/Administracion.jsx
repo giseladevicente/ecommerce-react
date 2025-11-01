977780014d165247dab3abfdac525ed749139750
giseladevicente <devicentegisela@gmail.com>
2025-11-01 10:28:08 -0300

    Agrego nombre de usuario y ajustes en la card

--- src/pages/Administracion.jsx
+++ src/pages/Administracion.jsx
@@ -1,21 +1,21 @@
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
-import Main from "../components/Main";
 
 export default function Administracion() {
   return (
     <>
-      <Main />
       <Container className="mt-4 d-flex flex-column align-items-center">
         <Card className="m-4 shadow-lg p-4 w-100" style={{ maxWidth: "600px" }}>
+          <Card.Header className="text-center">
+            Acceso exclusivo para usuarios autenticados.
+          </Card.Header>
           <Card.Body className="text-center">
             <Card.Title className="mb-3 fs-2">
               Panel de Administración
             </Card.Title>
 
-            <Card.Text className="mb-3 fs-5">
-              Acceso exclusivo para usuarios autenticados.
+            <Card.Text className="mb-3 fs-3 text-muted">
+              Bienvenido/a, {localStorage.getItem("usuario")}
             </Card.Text>
-
             <Row className="mb-3 g-3">
               <Col>
                 <Button variant="success" className="w-100">
@@ -31,10 +31,6 @@ export default function Administracion() {
                 </Button>
               </Col>
             </Row>
-
-            <Button variant="secondary" className="w-50">
-              Cerrar Sesión
-            </Button>
           </Card.Body>
         </Card>
       </Container>
